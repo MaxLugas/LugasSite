4816d15b0c06b7c4778922f3222b75c64fab2617
Lugas <maxim.lugovsky@gmail.com>
2023-01-24 19:32:59 +0300

    favicon.ico

--- LugasSite/LugasSite/urls.py
+++ LugasSite/LugasSite/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('main/img/favicon.ico'))),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('project/', include('news.urls')),
